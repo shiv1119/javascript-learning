5ccc59fd1bfbe03d19e6d7fb826259f2f73aede4
Shiv Nandan Verma <shivnandanverma63@gmail.com>
2022-08-21 13:04:56 +0530

    added some more files

---
+++ jstutorial/js1-writing-javascript.js
@@ -0,0 +1,10 @@
+/*    ------- practice in console 
+
+console.log("hello!!!")
+console.error("uhh ohh error!")
+
+>> There are two ways we can write javascript 
+1>> in brand new file  .js
+2>> In html file under <script> tag
+
+*/
